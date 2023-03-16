df7dac45adb6e70537790e09947ad281ae33854e
stoker <rodrigo.chuque.48@hotmail.com>
2023-03-16 00:28:31 -0500

    denuevo el bucle for

---
+++ untitled/universidadjava/src/fase3/bucle2s.java
@@ -0,0 +1,14 @@
+package fase3;
+
+public class bucle2s {
+
+
+
+        public static void main(String[] args) {
+            for(int contador = 0; contador <= 10; ++contador) {
+                System.out.println(contador);
+            }
+
+        }
+    }
+
