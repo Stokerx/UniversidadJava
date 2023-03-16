973f6668742c183e89a179223d5e1f2ace706ee4
stoker <rodrigo.chuque.48@hotmail.com>
2023-03-15 23:44:13 -0500

    bucle do while parte 1

--- untitled/universidadjava/src/fase3/bucless1.java
+++ untitled/universidadjava/src/fase3/bucless1.java
@@ -2,7 +2,12 @@ package fase3;
 
 public class bucless1 {
     public static void main(String[] args) {
-        System.out.println(" hola mundo ");
+       // do while
 
+        var contador = 0;
+        do{
+            System.out.println("contador + " + contador); // por lo menos se imprime una vez
+            contador++;
+        }while (contador< 0) ;
     }
 }
