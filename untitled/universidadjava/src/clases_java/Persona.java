acedae422231f40784c70df985170f0f92da77ed
stoker <rodrigo.chuque.48@hotmail.com>
2023-03-17 23:43:29 -0500

    parte de clases en java terminado

---
+++ untitled/universidadjava/src/clases_java/Persona.java
@@ -0,0 +1,19 @@
+package clases_java;
+
+public class Persona {
+    String nombre;
+    String apellido;
+
+    public Persona() { //constructor
+    }
+
+    public void desplegarInformacion(){
+        System.out.println("Nombre = "+ nombre);
+        System.out.println("apellido = " + apellido);
+
+    }
+
+
+
+
+}
